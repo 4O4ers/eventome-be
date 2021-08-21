9088c94d507d687030be172a2bad3b92876c1dec
adhammhaydat <adhammohidat123@gmail.com>
2021-08-21 12:01:26 +0300

    add favorit schema

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
-const cors = require('cors');
-app.use(cors())
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/event', {useNewUrlParser: true, useUnifiedTopology: true});
-app.use(express.json())
-const {getUser,postUser}=require('./controller/user.controller')
-const PORT=process.env.PORT
+const express = require("express"); // require the express package
+const app = express(); // initialize your express app instance
+const cors = require("cors");
+app.use(cors());
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/event", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(express.json());
+const {
+  getUser,
+  postUser,
+  eventHander,
+} = require("./controller/user.controller");
+const PORT = process.env.PORT;
 
+// a server endpoint
+app.get("/get", getUser);
+app.post("/post-user", postUser);
+app.post("/event", eventHander);
 
-
-// a server endpoint 
-app.get('/get',getUser)
-app.post('/post-user',postUser)
- 
-app.listen(PORT,()=>{
-    console.log(`working in port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`working in port ${PORT}`);
+});
